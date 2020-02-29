1530068e886069809ad335f816eaffa8435f815e
Kenneth LeFebvre <kenneth@lefebvre.us>
2020-02-28 23:14:57 -0500

    First commit of Sample API with Application Insights configured.

---
+++ src/ApiMiddleware/RequestLogging.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.ApplicationInsights;
+using Microsoft.ApplicationInsights.DataContracts;
+using Microsoft.ApplicationInsights.Extensibility;
+using Microsoft.AspNetCore.Http;
+
+namespace Kenlefeb.Api.Middleware
+{
+    public class RequestLogging
+    {
+        private readonly TelemetryClient _telemetry;
+        readonly RequestDelegate _next;
+
+        public RequestLogging(RequestDelegate next, TelemetryClient telemetry)
+        {
+            _next = next ?? throw new ArgumentNullException(nameof(next));
+        }
+
+        public async Task Invoke(HttpContext httpContext)
+        {
+            using (var operation = _telemetry.StartOperation<RequestTelemetry>("BPM Middleware Request"))
+            {
+                if (httpContext == null) throw new ArgumentNullException(nameof(httpContext));
+
+                var before = httpContext.Response.Body;
+                var body = default(string);
+
+                var start = Stopwatch.GetTimestamp();
+                var elapsed = default(double);
+                try
+                {
+                    using (var memory = new MemoryStream())
+                    {
+                        httpContext.Response.Body = memory;
+
+                        await _next(httpContext);
+                        elapsed = GetElapsedMilliseconds(start, Stopwatch.GetTimestamp());
+
+                        memory.Position = 0;
+                        body = new StreamReader(memory).ReadToEnd();
+                        memory.Position = 0;
+
+                        await memory.CopyToAsync(before);
+                    }
+
+                    var statusCode = httpContext.Response?.StatusCode;
+                    var level = statusCode > 499 ? LogLevel.Error : LogLevel.Information;
+
+                    var request = httpContext.Request;
+                    var response = httpContext.Response;
+
+                    var properties = CollectProperties(httpContext);
+                    if (!string.IsNullOrEmpty(body))
+                        properties.Add("Response", body);
+
+                    var metrics = new Dictionary<string, double>
+                                  {
+                                      {"Duration", elapsed},
+                                  };
+                    this._telemetry.TrackEvent("HTTP Request", properties, metrics);
+                }
+                // Never caught, because `LogException()` returns false.
+                catch (Exception ex)
+                    when (LogException(httpContext, GetElapsedMilliseconds(start, Stopwatch.GetTimestamp()), ex))
+                { }
+                finally
+                {
+                    httpContext.Response.Body = before;
+                }
+            }
+        }
+
+        private static Dictionary<string, string> CollectProperties(HttpContext httpContext)
+        {
+            var request = httpContext.Request;
+            var response = httpContext.Response;
+
+            var properties = new Dictionary<string, string>();
+
+            foreach (var header in request.Headers)
+                properties.Add($"Request: {header.Key}", $"{header.Value}");
+
+            foreach (var header in response.Headers)
+                properties.Add($"Response: {header.Key}", $"{header.Value}");
+
+            properties.Add("Method", request.Method);
+            properties.Add("Path", GetPath(httpContext));
+            properties.Add("Host", $"{request.Host}");
+            properties.Add("Protocol", request.Protocol);
+            properties.Add("StatusCode", $"{(int)response.StatusCode} {response.StatusCode}");
+
+            var body = GetRequestBody(request);
+            if (!string.IsNullOrEmpty(body))
+                properties.Add("Request", body);
+
+            if (request.Query.Any())
+            {
+                foreach (var parameter in request.Query)
+                {
+                    properties.Add($"Query: {parameter.Key}", parameter.Value);
+                }
+            }
+
+            return properties;
+        }
+
+        private static string GetRequestBody(HttpRequest request)
+        {
+            try
+            {
+                request.EnableBuffering();
+                var before = request.Body.Position;
+                request.Body.Position = 0;
+                using (var reader = new StreamReader(request.Body))
+                {
+                    var body = reader.ReadToEnd();
+                    request.Body.Position = before;
+                    return body;
+                }
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+        bool LogException(HttpContext httpContext, double elapsedMs, Exception ex)
+        {
+            var properties = CollectProperties(httpContext);
+            this._telemetry.TrackException(ex, properties);
+            return false;
+        }
+
+        static double GetElapsedMilliseconds(long start, long stop)
+        {
+            return (stop - start) * 1000 / (double)Stopwatch.Frequency;
+        }
+
+        static string GetPath(HttpContext httpContext)
+        {
+            return httpContext.Features.Get<IHttpRequestFeature>()?.RawTarget ?? httpContext.Request.Path.ToString();
+        }
+    }
+}
---
+++ src/Sample/Controllers/WeatherForecastController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.ApplicationInsights;
+using Microsoft.ApplicationInsights.DataContracts;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Sample.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+        private readonly TelemetryClient _telemetry;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger, TelemetryClient telemetry)
+        {
+            _logger = logger;
+            _telemetry = telemetry;
+            _logger.LogInformation($"Constructing the {nameof(WeatherForecastController)}");
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            using var operation = _telemetry.StartOperation<RequestTelemetry>("GET WeatherForecasts");
+            var rng = new Random();
+            var forecasts = Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+                .ToArray();
+            foreach (var forecast in forecasts)
+            {
+                _telemetry.TrackEvent($"Forecast: {forecast.Date}", new Dictionary<string, string>
+                                                                    {
+                                                                        {"Date", $"{forecast.Date:o}"},
+                                                                        {"Celsius", $"{forecast.TemperatureC}℃"},
+                                                                        {"Fahrenheit", $"{forecast.TemperatureC}℉"},
+                                                                        {"Summary", $"{forecast.Summary}"},
+                                                                    },
+                    new Dictionary<string, double>
+                    {
+                        {"Forecasts", 1},
+                        {"Temperature", forecast.TemperatureC }
+                    });
+            }
+            return forecasts;
+        }
+    }
+}
